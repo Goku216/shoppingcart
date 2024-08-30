af1d8253a70011bff0aa3374599754dfb47031be
Goku216 <sagarpariyar345@gmail.com>
2024-08-30 12:46:16 -0400

    fixed cart number

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,21 @@ export const Card = ({ cart, setCart }) => {
   const products = useFetch("http://localhost:3000/products");
 
   const addToCart = (product) => {
-    setCart((prevCart) => [...prevCart, product]);
+    setCart((prevCart) => {
+      // Check if prevCart is an array before proceeding
+      if (Array.isArray(prevCart)) {
+        const isAlreadyInCart = prevCart.some((item) => item.id === product.id);
+
+        if (!isAlreadyInCart) {
+          return [...prevCart, product];
+        } else {
+          return prevCart;
+        }
+      } else {
+        // If prevCart is not an array, initialize it as an array
+        return [product];
+      }
+    });
   };
 
   return (
@@ -25,7 +39,7 @@ export const Card = ({ cart, setCart }) => {
                 <span className="text-lg font-medium">${product.price}</span>
                 <span
                   onClick={() => addToCart(product)}
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-normal rounded-md text-md p-2 m-1"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-normal rounded-md text-md p-2 m-1 cursor-pointer"
                 >
                   Add To Cart
                 </span>
